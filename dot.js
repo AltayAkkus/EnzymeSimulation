f612f69fe0f008c50b2a8fe89815daac50a0e122
AltayAkkus <altayakkus1993@googlemail.com>
2020-04-27 19:56:20 +0200

    fsd

---
+++ dot.js
@@ -0,0 +1,66 @@
+'use strict'
+
+class Dot {
+	//Constructs shape
+	constructor(canvas, size, enzyme, reacted, active) {
+		this.pos = startLocation(canvas.x, canvas.y)
+		this.speed = randCoordinate()
+		this.direction = randCoordinate()
+		//Checks if dot is enzyme, if so changes color
+		this.colour = enzyme ? color(255, 0, 0) : color(255, 255, 255)
+		this.canvas = canvas
+		this.size = size
+		this.enzyme = enzyme
+		this.reacted = reacted
+		this.active = active
+	}
+
+	move() {
+		this.checkBoundaries()
+		this.pos = new Coordinate(this.pos.x + this.speed.x * this.direction.x, this.pos.y + this.speed.y * this.direction.y)
+	}
+	//If the shape collides with a wall, the direction is reverted
+	checkBoundaries() {
+		if (hitBoundary(this, 'x')) {
+			this.direction.x *= -1
+		}
+		if (hitBoundary(this, 'y')) {
+			this.direction.y *= -1
+		}
+	}
+	//Renders a circle
+	render() {
+		ellipse(this.pos.x, this.pos.y, this.size)
+	}
+	//TODO: Delete
+	infect() {
+		this.infected = true
+		this.colour = color(255, 0, 0)
+	}
+	collide() {
+		this.reacted = true
+		//TODO: Delete object
+	}
+
+
+}
+
+function hitBoundary(dot, axis) {
+	return dot.pos[axis] > dot.canvas[axis] - dot.size || dot.pos[axis] < dot.size 
+}
+
+function startLocation(x, y) {
+	return new Coordinate(randRange(x, 5), randRange(y, 5))
+}
+
+function randRange(max, min) {
+	return Math.ceil(Math.random() * (max - min) + min)
+}
+
+function rand(constraint) {
+	return Math.ceil(Math.random() * constraint)
+}
+
+function randCoordinate() {
+	return new Coordinate(rand(2), rand(2))
+}
---
+++ sketch.js
@@ -0,0 +1,116 @@
+'use strict'
+
+const width = window.innerWidth
+const height = 800
+
+const population = 100
+
+const dots = []
+let reactions = 0
+const enzymes = []
+
+function setup() {
+	createCanvas(width, height)
+	createPopulation()
+}
+
+function draw() {
+	background(51)
+	moveDots()
+	updateText()
+}
+
+function updateText() {
+	select('#reactions').html(`Reaktionen: ${reactions.length}`)
+}
+
+/*function checkComplete() {
+	if (dots.length - infected.length == 0) {
+		noLoop()
+	}
+}*/
+
+function moveDots() {
+	for (const dot of dots) {
+		dot.move()
+		fill(dot.colour)
+		dot.render()
+		for (const dot2 of dots) {
+			if(dot.active) {
+				checkCollision(dot, dot2)
+			}
+		}
+	}
+}
+//Checks if entitys collide
+/*function checkCollision(dot, dot2) {
+	//Calculates distance between dot and dot2, checks if the distance between is smaller than the size of the dots -> collision happened
+	//If so checks if one of the dots is infected, then infects the other.
+	if (dist(dot.pos.x, dot.pos.y, dot2.pos.x, dot2.pos.y) < dot.size 
+		&& isOneInfected(dot, dot2)) {
+		dot.infect()
+	 	dot2.infect()
+		infected.push(dot.infected)
+	}
+}*/
+function checkCollision(dot, dot2) {
+	//Calculates distance between dot and dot2, checks if the distance between is smaller than the size of the dots -> collision happened
+	//If so checks if one of the dots is infected, then infects the other.
+	if (dist(dot.pos.x, dot.pos.y, dot2.pos.x, dot2.pos.y) < dot.size 
+		&& isOneEnzyme(dot, dot2)) {
+		console.log("Collision!")
+		if(dot.enzyme) {
+			//Do nothing to the enzyme
+			deleteEntity(dot2)
+			if(dot.reacted) {
+				//Enzyme completed reaction
+				reactions += 1 
+				dot.reacted = false 
+			} else {
+				dot.reacted = true 
+			}
+		} else {
+			deleteEntity(dot)
+			if(dot2.reacted) {
+				//Enzyme completed reaction
+				reactions += 1 
+				dot2.reacted = false 
+			} else {
+				dot2.reacted = true 
+			}
+		}
+	}
+}
+function isOneEnzyme(dot, dot2) {
+	//Returns if one of the dots is an and enzyme, or if both are enzymes
+	return (dot.enzyme || dot2.enzyme) && !areBothEnzymes(dot, dot2)
+}
+function areBothEnzymes(dot, dot2) {
+	//Returns if both entitys are enzymes
+	return dot.enzyme && dot2.enzyme
+}
+function deleteEntity(dot) {
+	dot.colour = 
+	dot.active = false;
+}
+/*
+function isOneInfected(dot, dot2) {
+	return (dot.infected || dot2.infected) && !areBothInfected(dot, dot2)
+}
+
+function areBothInfected(dot, dot2) {
+	return dot.infected && dot2.infected
+}
+*/
+function createPopulation() {
+ 	for (let i = 1; i < population; i++) {
+		dots.push(new Dot(new Coordinate(width, height), 16, false, false, true))
+	}
+	createEnzyme()
+}
+
+function createEnzyme() {
+	const enzyme = new Dot(new Coordinate(width, height), 16, true, false, true)
+	dots.push(enzyme)
+	enzymes.push(enzyme)
+}
